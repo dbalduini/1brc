4958812b7df596f1afbb7199964f7a642b133a9e
Diego Balduini <diego.balduini@cognira.com>
2024-04-11 17:38:41 -0300

    revert to string because it was faster

--- src/lib.rs
+++ src/lib.rs
@@ -8,38 +8,16 @@ pub mod measurement;
 pub fn parse_file(path: &str) -> StationsMap {
     let mut map = StationsMap::new();
 
-    let buffer = fs::read(path).unwrap();
+    let content = fs::read_to_string(path).unwrap();
 
-    let mut i = 0;
-    let mut j = 0;
-    for byte in buffer.iter() {
-        // new line
-        if *byte == b'\n' {
-            process_line(&buffer[i..j], &mut map);
-            i = j + 1;
-        }
-        j += 1;
+    for line in content.lines() {
+        process_line(line, &mut map);
     }
-
-    // let mut reader = BufReader::new(file);
-    // while let Ok(n) = reader.read_until(b'\n', &mut buf) {
-    //     if n == 0 {
-    //         // EOF
-    //         break;
-    //     }
-
-    //     let line = &buf[..n - 1];
-
-    //     process_line(line, &mut map);
-
-    //     buf.clear();
-    // }
 
     map
 }
 
-fn process_line(line: &[u8], map: &mut StationsMap) {
-    let line = unsafe { String::from_utf8_unchecked(line.to_vec()) };
+fn process_line(line: &str, map: &mut StationsMap) {
     if let Some((station, t)) = line.split_once(";") {
         let t = t.parse::<f64>().unwrap();
         map.upsert(station, t);
