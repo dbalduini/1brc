5c2344f426f80eff7842bd9510a74f119060abfa
Diego Balduini <diego.balduini@cognira.com>
2024-04-12 17:47:57 -0300

    fix number of workers

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,8 @@ pub fn aggregate_stations(path: &str) -> StationsMap {
     // let content = fs::read_to_string(path).unwrap();
 
     // returns the number of logical processors (hyperthreading)
-    // let num_workers = std::thread::available_parallelism().unwrap().get();
-    let num_workers = 1;
+    let num_workers = std::thread::available_parallelism().unwrap().get();
+    // let num_workers = 1;
     
     println!("using {} workers", num_workers);
     let mut pool = WorkerPool::new(num_workers);
